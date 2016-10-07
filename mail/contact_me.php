828dd736761da6cc805a02282a033572e9704046
monolok <antoinebertin@me.com>
2016-10-06 18:38:33 -0600

    testing mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,12 +15,33 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
-// Create the email and send the message
-$to = 'antoinebertin35@hotmail.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+// // Create the email and send the message
+
+// $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";   
+// mail($to,$email_subject,$email_body,$headers);
+// return true; 
+
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+
+// If you are using Composer
+require 'vendor/autoload.php';
+// If you are not using Composer (recommended)
+// require("path/to/sendgrid-php/sendgrid-php.php");
+$from = new SendGrid\Email("Example User", $email_address);
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "antoinebe35@gmail.com");
+$content = new SendGrid\Content($message);
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = getenv('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');
+$sg = new \SendGrid($apiKey);
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
+
 ?>
